26296360f6388c5bb237bb29d5a62abec96983f7
vamsi199 <vamsi.1991992@gmail.com>
2017-09-24 23:32:12 -0400

    Added main

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
-import (//"github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"log"
 )
 
-func main()  {
-	//router := mux.NewRouter()
-	//router.HandleFunc("/upload?posy=1&hel=1")
-	Uploadobject("hello","jenkins19")
-
+func main() {
+	//Uploadobject("hello", "jenkins19","now")
+	//Listobject("jenkins19")
+	//Downloadobj("now","jenkins19")
+	//Deleteobj("now","jenkins19")
+	router := mux.NewRouter()
+	router.HandleFunc("/uploadobj" ,Uploadobject).Methods("PUT")//(/uploadobj?filename=&bucketname=&objectname=)
+	router.HandleFunc("/listobj" ,Listobject).Methods("GET") //(/listobj?bucketname=)
+	router.HandleFunc("/downloadobj" ,Downloadobj).Methods("GET") //(/downloadobj?bucketname=&objectname=)
+	router.HandleFunc("/deleteobj" ,Deleteobj).Methods("DELETE") //(/deleteobj?bucketname=&objectname=)
+	log.Fatal(http.ListenAndServe(":8081", router))
 }
+
--- s3func.go
+++ s3func.go
@@ -140,15 +140,3 @@ func Downloadobj(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w,"new file downloaded:", n)
 
 }
-func main() {
-	//Uploadobject("hello", "jenkins19","now")
-	//Listobject("jenkins19")
-	//Downloadobj("now","jenkins19")
-	//Deleteobj("now","jenkins19")
-	router := mux.NewRouter()
-	router.HandleFunc("/uploadobj" ,Uploadobject).Methods("PUT")//(/uploadobj?filename=&bucketname=&objectname=)
-	router.HandleFunc("/listobj" ,Listobject).Methods("GET") //(/listobj?bucketname=)
-	router.HandleFunc("/downloadobj" ,Downloadobj).Methods("GET") //(/downloadobj?bucketname=&objectname=)
-	router.HandleFunc("/deleteobj" ,Deleteobj).Methods("DELETE") //(/deleteobj?bucketname=&objectname=)
-	log.Fatal(http.ListenAndServe(":8081", router))
-}
